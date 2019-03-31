ad559933603c1bdf89e8b6afe043e9d129ae0ce6
Sava.Katic <savakatic123@hotmail.com>
2019-03-31 17:50:07 +0200

    app shows correct data for separate 5 days now

--- WeatherApp/WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -56,74 +56,92 @@ namespace WeatherApp
                 getWeather(string.Format("{0}", result.city));
             }
         }
+
+        int GetMaximumTemp(int start, int end, WeatherInfo.List[] list)
+        {
+            int max = 0;
+            for(int i = start; i < end; i++)
+            {
+                int value = (int)list[i].main.temp_max;
+                if (value > max)
+                {
+                    max = value;
+                }
+            }
+            return max;
+        }
+
+        int GetMinimumTemp(int start, int end, WeatherInfo.List[] list)
+        {
+            int min = 0;
+            for (int i = start; i < end; i++)
+            {
+                int value = (int)list[i].main.temp_min;
+                if (value < min)
+                {
+                    min = value;
+                }
+            }
+            return min;
+        }
  
         void getWeather(string city)
         {
             using(WebClient web = new WebClient())
             {
                 string cityName = city;
-                string url = string.Format("http://api.openweathermap.org/data/2.5/forecast?q={0}&appid={1}" + (this.TempSymbol.Equals("°F") ? "" : "&units=metric"), cityName,appid);
+                string url = string.Format("http://api.openweathermap.org/data/2.5/forecast?q={0}&appid={1}&units=" + (this.TempSymbol.Equals("°F") ? "imperial" : "metric"), cityName,appid);
                 var json = web.DownloadString(url);
 
                 var result = JsonConvert.DeserializeObject<WeatherInfo.Rootobject>(json);
                 WeatherInfo.Rootobject output = result;
                 
-
                 //grad, drzava, trenutna temperatura
                 label_CityName.Content = string.Format("{0}", output.city.name);
                 label_CountryName.Content = string.Format("{0}", output.city.country);
                 label_Temperature.Content = string.Format("{0} " + this.TempSymbol, Convert.ToInt64(Math.Floor(Convert.ToDouble(output.list[0].main.temp))));
 
-                // uzima vrednosti za datum i vreme
-                label_3.Content = string.Format("{0}", output.list[0].dt_txt);
-                label_6.Content = string.Format("{0}", output.list[1].dt_txt);
-                label_9.Content = string.Format("{0}", output.list[2].dt_txt);
-                label_12.Content = string.Format("{0}", output.list[3].dt_txt);
-                label_15.Content = string.Format("{0}", output.list[4].dt_txt);
-                label_18.Content = string.Format("{0}", output.list[5].dt_txt);
-                label_21.Content = string.Format("{0}", output.list[6].dt_txt);
-                label_24.Content = string.Format("{0}", output.list[7].dt_txt);
+                int hour = Int32.Parse(output.list[0].dt_txt.Split(' ')[1].Split(':')[0]);
+     
+                int secondDayStartIndex = (24 - hour) / 3 == 0 ? 8 : (24 - hour) / 3;
+
+                // uzima vrednosti za datum
+                label_1.Content = string.Format("{0}", output.list[0].dt_txt.Split(' ')[0]);
+                label_2.Content = string.Format("{0}", output.list[secondDayStartIndex].dt_txt.Split(' ')[0]);
+                label_3.Content = string.Format("{0}", output.list[secondDayStartIndex + 8].dt_txt.Split(' ')[0]);
+                label_4.Content = string.Format("{0}", output.list[secondDayStartIndex + 16].dt_txt.Split(' ')[0]);
+                label_5.Content = string.Format("{0}", output.list[secondDayStartIndex + 24].dt_txt.Split(' ')[0]);
+
+                int firstDayMiddle = secondDayStartIndex / 2 >= 4 ? secondDayStartIndex / 2 : 0;
+                int secondDayMiddle = secondDayStartIndex + 4;
 
                 //Ikonice
-                Img3.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[0].weather[0].icon + ".png"));
-                Img6.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[1].weather[0].icon + ".png"));
-                Img9.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[2].weather[0].icon + ".png"));
-                Img12.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[3].weather[0].icon + ".png"));
-                Img15.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[4].weather[0].icon + ".png"));
-                Img18.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[5].weather[0].icon + ".png"));
-                Img21.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[6].weather[0].icon + ".png"));
-                Img24.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[7].weather[0].icon + ".png"));
-
-                //temperature na svaka tri sata
-                label_3T.Content = string.Format("{0} " + this.TempSymbol, output.list[1].main.temp); //3 hours from now
-                label_6T.Content = string.Format("{0} " + this.TempSymbol, output.list[2].main.temp); //6 hours from now
-                label_9T.Content = string.Format("{0} " + this.TempSymbol, output.list[3].main.temp); //9 hours from now
-                label_12T.Content = string.Format("{0} " + this.TempSymbol, output.list[4].main.temp); //12 hours from now
-                label_15T.Content = string.Format("{0} " + this.TempSymbol, output.list[5].main.temp); //15 hours from now
-                label_18T.Content = string.Format("{0} " + this.TempSymbol, output.list[6].main.temp); //18 hours from now
-                label_21T.Content = string.Format("{0} " + this.TempSymbol, output.list[7].main.temp); //21 hours from now
-                label_24T.Content = string.Format("{0} " + this.TempSymbol, output.list[8].main.temp); //24 hours from now
-
+                Img1.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[firstDayMiddle].weather[0].icon + ".png"));
+                Img2.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[secondDayMiddle].weather[0].icon + ".png"));
+                Img3.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[secondDayMiddle + 8].weather[0].icon + ".png"));
+                Img4.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[secondDayMiddle + 16].weather[0].icon + ".png"));
+                Img5.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/" + output.list[secondDayMiddle + 24].weather[0].icon + ".png"));
+                
+                //temperature
+                label_1T.Content = string.Format("{0} " + this.TempSymbol, this.GetMaximumTemp(0, secondDayStartIndex, output.list));
+                label_2T.Content = string.Format("{0} " + this.TempSymbol, this.GetMaximumTemp(secondDayStartIndex, secondDayStartIndex + 8, output.list));
+                label_3T.Content = string.Format("{0} " + this.TempSymbol, this.GetMaximumTemp(secondDayStartIndex + 8, secondDayStartIndex + 16, output.list));
+                label_4T.Content = string.Format("{0} " + this.TempSymbol, this.GetMaximumTemp(secondDayStartIndex + 16, secondDayStartIndex + 24, output.list));
+                label_5T.Content = string.Format("{0} " + this.TempSymbol, this.GetMaximumTemp(secondDayStartIndex + 24, secondDayStartIndex + 32, output.list));
+                
                 //vetar
-                label_3Wind.Content = string.Format("{0} km/h", output.list[1].wind.speed); 
-                label_6Wind.Content = string.Format("{0} km/h", output.list[2].wind.speed); 
-                label_9Wind.Content = string.Format("{0} km/h", output.list[3].wind.speed); 
-                label_12Wind.Content = string.Format("{0} km/h", output.list[4].wind.speed); 
-                label_15Wind.Content = string.Format("{0} km/h", output.list[5].wind.speed); 
-                label_18Wind.Content = string.Format("{0} km/h", output.list[6].wind.speed); 
-                label_21Wind.Content = string.Format("{0} km/h", output.list[7].wind.speed); 
-                label_24Wind.Content = string.Format("{0} km/h", output.list[8].wind.speed); 
+                label_1Wind.Content = string.Format("{0} " + (this.TempSymbol.Equals("°C") ? "m/s" : "mph"), output.list[firstDayMiddle].wind.speed); 
+                label_2Wind.Content = string.Format("{0} " + (this.TempSymbol.Equals("°C") ? "m/s" : "mph"), output.list[secondDayMiddle].wind.speed); 
+                label_3Wind.Content = string.Format("{0} " + (this.TempSymbol.Equals("°C") ? "m/s" : "mph"), output.list[secondDayMiddle + 8].wind.speed); 
+                label_4Wind.Content = string.Format("{0} " + (this.TempSymbol.Equals("°C") ? "m/s" : "mph"), output.list[secondDayMiddle + 16].wind.speed); 
+                label_5Wind.Content = string.Format("{0} " + (this.TempSymbol.Equals("°C") ? "m/s" : "mph"), output.list[secondDayMiddle + 24].wind.speed); 
 
                 //pritisak
-                label_3Pressure.Content = string.Format("{0} mbar", output.list[1].main.pressure);
-                label_6Pressure.Content = string.Format("{0} mbar", output.list[2].main.pressure);
-                label_9Pressure.Content = string.Format("{0} mbar", output.list[3].main.pressure);
-                label_12Pressure.Content = string.Format("{0} mbar", output.list[4].main.pressure);
-                label_15Pressure.Content = string.Format("{0} mbar", output.list[5].main.pressure);
-                label_18Pressure.Content = string.Format("{0} mbar", output.list[6].main.pressure);
-                label_21Pressure.Content = string.Format("{0} mbar", output.list[7].main.pressure);
-                label_24Pressure.Content = string.Format("{0} mbar", output.list[8].main.pressure);
-
+                label_1Pressure.Content = string.Format("{0} mbar", output.list[firstDayMiddle].main.pressure);
+                label_2Pressure.Content = string.Format("{0} mbar", output.list[secondDayMiddle].main.pressure);
+                label_3Pressure.Content = string.Format("{0} mbar", output.list[secondDayMiddle + 8].main.pressure);
+                label_4Pressure.Content = string.Format("{0} mbar", output.list[secondDayMiddle + 16].main.pressure);
+                label_5Pressure.Content = string.Format("{0} mbar", output.list[secondDayMiddle + 24].main.pressure);
 
             }
             
@@ -160,37 +178,6 @@ namespace WeatherApp
                     getWeather(chosenCity);
             }
         }
-
-
-        /*  private void Label_CityName_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-          {
-              label_monT.Visibility = Visibility.Visible;
-              label_tueT.Visibility = Visibility.Visible;
-              label_wedT.Visibility = Visibility.Visible;
-              label_thuT.Visibility = Visibility.Visible;
-              label_friT.Visibility = Visibility.Visible;
-              label_satT.Visibility = Visibility.Visible;
-              label_sunT.Visibility = Visibility.Visible;
-
-              label_mon.Visibility = Visibility.Visible;
-              label_tue.Visibility = Visibility.Visible;
-              label_wed.Visibility = Visibility.Visible;
-              label_thu.Visibility = Visibility.Visible;
-              label_fri.Visibility = Visibility.Visible;
-              label_sat.Visibility = Visibility.Visible;
-              label_sun.Visibility = Visibility.Visible;
-
-
-              monImg.Visibility = Visibility.Visible;
-              tueImg.Visibility = Visibility.Visible;
-              wedImg.Visibility = Visibility.Visible;
-              thuImg.Visibility = Visibility.Visible;
-              friImg.Visibility = Visibility.Visible;
-              satImg.Visibility = Visibility.Visible;
-              sunImg.Visibility = Visibility.Visible;
-
-          }
-          */
     }
 }
 
